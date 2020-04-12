cce3324fe779f50ad3f58458374cd33a4fd95444
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2020-04-12 00:44:59 -0700

    changing connection string

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports={
 
     //"URI":"mongodb://localhost/test"
-    "URI" : "mongodb+srv://hemrajsinh:1234@comp308lesson4-c5tuh.azure.mongodb.net/test?retryWrites=true&w=majority"
+    "URI" : "mongodb://hemrajsinh:1234@comp308lesson4-c5tuh.azure.mongodb.net/test?retryWrites=true&w=majority"
 }
