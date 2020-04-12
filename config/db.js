39b0192506008b8aee81d5950dd547fc82ab5316
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2020-04-12 05:25:07 -0700

    Create contacts in DB

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports={
 
-   // "URI":"mongodb://localhost/test"
-    "URI" : "mongodb+srv://hemraj:a1234@cluster0-9ay0c.azure.mongodb.net/test?retryWrites=true&w=majority"
+    "URI":"mongodb://localhost/test"
+    //"URI" : "mongodb+srv://hemraj:a1234@cluster0-9ay0c.azure.mongodb.net/test?retryWrites=true&w=majority"
 }
--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,11 @@ let router = express.Router();
 let mongoose=require('mongoose');
 
 //create a reference to db schema
-let contact=require("../models/contact");
+let contactModel=require("../models/contact");
 
 /*GET contact list page- READ operation */
 router.get('/',(req, res, next)=>{
-    contact.find((err, contactList)=>{
+    contactModel.find((err, contactList)=>{
         if(err)
         {
             return console.error(err);
@@ -19,6 +19,36 @@ router.get('/',(req, res, next)=>{
             title:'Contact List',
             contactList:contactList
         }); 
+        }
+    });
+});
+
+//GET route for Add page adn will display add page
+router.get('/add',(req, res, next)=>{
+    res.render('contacts/add',{
+        title:'Add New Contact'
+    });
+});
+
+//POST routes for Add Contact
+router.post('/add',(req, res, next)=>{
+
+    let newContact=contactModel({
+        "firstName" : req.body.firstName,
+        "lastName" : req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.create(newContact, (err, contactModel)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh contact list and redirect to the page
+            res.redirect('/contact-list');
         }
     });
 });
