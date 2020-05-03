b46bcd042f9eeb5eb368d210e0fe2ce41d012964
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2020-05-03 02:19:52 -0700

    edit and delete contact with jquery confirm code

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,6 +4,13 @@
 
     function Start(){
         console.log(`%c Apllication started`,"font-size:20px; color:blue;font-weight:bold;");
+
+        $(".btn-danger").click(function(event){
+            if(!confirm("Are you sure????")){
+                event.preventDefault();
+                window.location.assign("/contact-list");
+            }
+        });
     }
     addEventListener("load", Start);
 })();
--- routes/contact.js
+++ routes/contact.js
@@ -53,4 +53,64 @@ router.post('/add',(req, res, next)=>{
     });
 });
 
+//Get request for edit contact
+router.get('/edit/:id', (req, res, next)=>{
+    let id=req.params.id;
+
+    contactModel.findById(id, (err, contactObject)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // show edit view
+            res.render('contacts/edit',{
+                title:'Edit Contact',
+                contact:contactObject
+            });
+        }
+    });
+
+});
+//post request for edit method
+router.post('/edit/:id',(req, res, next)=>{
+    let id=req.params.id;
+
+    let updatedContact=contactModel({
+        "_id":id,
+        "firstName":req.body.firstName,
+        "lastName":req.body.lastName,
+        "age":req.body.age
+    });
+
+    contactModel.update({_id:id}, updatedContact, (err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/contact-list');
+        }
+    })
+});
+//Get method for delete request
+router.get('/delete/:id',(req, res, next)=>{
+    let id=req.params.id;
+    contactModel.remove({_id:id}, (err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/contact-list')
+        }
+    })
+})
+
 module.exports = router;
