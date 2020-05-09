ea978db36746b58c23c6535cffe1bbd6fdbe955c
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2020-05-09 09:25:44 -0700

    change connection string for mongodbAtlas and heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports={
 
-    "URI":"mongodb://localhost/test"
-  //  "URI" : "mongodb+srv://hemrajsinh:a12345@comp308lesson4-c5tuh.azure.mongodb.net/test?retryWrites=true&w=majority"
+  //  "URI":"mongodb://localhost/test"
+    "URI" : "mongodb+srv://hemrajsinh:a12345@comp308lesson4-c5tuh.azure.mongodb.net/test?retryWrites=true&w=majority"
 }
